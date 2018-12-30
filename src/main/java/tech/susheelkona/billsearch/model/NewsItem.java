29f5b1925c4171c854eec54fb952ac0dc5c236d9
susheelk <susheel.kona@gmail.com>
2018-12-30 02:20:41 -0500

    Made news plain English

--- src/main/java/tech/susheelkona/billsearch/model/NewsItem.java
+++ src/main/java/tech/susheelkona/billsearch/model/NewsItem.java
@@ -59,6 +59,36 @@ public class NewsItem {
     }
 
     public void generateTagline() {
-        setTagline(billNumber+", "+title);
+        String tag = "";
+
+        switch (title){
+            case "Concurrence in Senate Amendments in the House of Commons":
+                tag = "House of Commons adopts Senate Amendments to Bill " + billNumber;
+                break;
+
+            case "Consideration of Senate Amendments in the House of Commons":
+                tag = "House of Commons considers Senate Amendments to Bill " + billNumber;
+                break;
+
+            case "Royal Assent in the Senate":
+                tag = "Bill " + billNumber + " receives Royal Assent, becomes law";
+                break;
+
+            default: tag = billNumber+", "+title;
+        }
+
+        if (title.contains("Introduction and")) {
+            int aInd = title.indexOf("and");
+            int rInd = title.indexOf("Reading");
+            String time = title.substring(aInd+4, rInd-1);
+            String chamb = title.substring(rInd+8);
+            tag = "Bill "+ billNumber + " introduced " + chamb;
+        } else  if (title.contains("Reading in the")) {
+            int rInd = title.indexOf("Reading");
+            String time = title.substring(0, rInd-1);
+            String chamb = title.substring(rInd+8);
+            tag = "Bill " + billNumber + " read for the " + time + " time " + chamb;
+        }
+        setTagline(tag);
     }
 }
