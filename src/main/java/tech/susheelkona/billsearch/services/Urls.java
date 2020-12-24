1fb75b5a63b45295ab8cafe98e0d416a853523b8
Susheel Kona <susheel.kona@gmail.com>
2020-12-23 22:22:59 -0500

    new session

--- src/main/java/tech/susheelkona/billsearch/services/Urls.java
+++ src/main/java/tech/susheelkona/billsearch/services/Urls.java
@@ -1,7 +1,7 @@
 package tech.susheelkona.billsearch.services;
 
 public interface Urls {
-    String LEGISINFO_LATEST_ORDER = "https://www.parl.ca/LegisInfo/Home.aspx?Language=E&Mode=1&SortBy=BillLatestEventStartTime&SortDir=DESC&ParliamentSession=43-1&Page=1&download=xml";
+    String LEGISINFO_LATEST_ORDER = "https://www.parl.ca/LegisInfo/Home.aspx?Language=E&Mode=1&SortBy=BillLatestEventStartTime&SortDir=DESC&ParliamentSession=43-2&Page=1&download=xml";
     String LEGISINFO_VOTES = "https://www.ourcommons.ca/members/en/votes/xml";
     String LEGISINFO_BALLOT = "https://www.ourcommons.ca/Parliamentarians/en/HouseVotes/ExportDetailsVotes?output=XML&parliament=43&session=1&vote=";
     String LEGISINFO_VOTE_HTML = "https://www.ourcommons.ca/Parliamentarians/en/votes/43/1/";
